28d7490e32c500c4f8e1f029358323859e99fdfe
Hungju Lu <jefflen@gmail.com>
2016-12-01 15:23:00 +0000

    Initial Commit

---
+++ CoreDataPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataPractice
+//
+//  Created by Hungju Lu on 01/12/2016.
+//  Copyright © 2016 Hungju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
